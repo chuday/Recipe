c7180ad60b909e40ffaf1dc550fb8e42311a00c9
chuday <mikhail_chudaew505@mail.ru>
2023-06-23 12:20:08 +0500

    Create app

---
+++ Recipe/Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Mikhail Chudaev on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
